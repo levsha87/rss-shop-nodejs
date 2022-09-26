86a999da94403d7743f52ae3420c5b5ff4562c95
Raman Liausha <Raman_Liausha@epam.com>
2022-09-26 23:49:45 +0300

    feat: add configuration for getProductsById function

--- product-service/handler.js
+++ product-service/handler.js
@@ -1,15 +1,16 @@
 'use strict';
+
 const products = [{
   title: "Canon",
   model: "SX120IS",
   price: "$80",
-  id: 1
+  id: "1"
 },
 {
   title: "Sony",
   model: "X25",
   price: "$45",
-  id: 2
+  id: "2"
 }];
 
 module.exports.getProductsList = async (event) => {
@@ -25,16 +26,15 @@ module.exports.getProductsList = async (event) => {
 };
 
 module.exports.getProductsById = async (event) => {
-  const pathname = window.location.pathname;
-  const splitPath = pathname.split("/");
-  const currentId = splitPath[splitPath.length-1];
-
-  console.log('current id', currentId);
+  const currentId = event.pathParameters.id;
+  const [product] = products.filter((product) => {
+    return product.id === currentId;
+  });
 
   return {
     statusCode: 200,
     body: JSON.stringify(
-      products.filter((product) => product.id === currentId),
+      product,
       null,
       2
     ),
